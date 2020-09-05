1007eba6cf5bdd798cb47938ec01e119b26acfac
changzehai <changzehai@yahoo.co.jp>
2020-09-05 22:39:08 +0800

    收到

---
+++ 20200905/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _20200905
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 100;
+            int b = 200;
+
+            int c = a *b;
+            Console.WriteLine(c);
+        }
+    }
+}
